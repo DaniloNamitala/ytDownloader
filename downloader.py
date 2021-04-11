aae799288b431bbbfa9025e8b6c67a2b01e5d485
Danilo Namitala <danilonamitala@yahoo.com.br>
2021-04-11 17:32:26 -0300

    added multithreading

--- downloader.py
+++ downloader.py
@@ -7,7 +7,7 @@ import os
 import sys
 import math
 import argparse
-
+import concurrent.futures
 #Parse arguments
 def create_parser():
     parser = argparse.ArgumentParser()
@@ -15,6 +15,7 @@ def create_parser():
     parser.add_argument('url', help='Video or playlsit url')
     parser.add_argument('path',type=Path, help='The destination path', default="./")
     parser.add_argument('-a','--audio', help='Download only the audio', action="store_true")
+    parser.add_argument('-mt','--multithreading', type=int, help="Number of threads for downloading", default=1)
     return parser.parse_args()
 
 #Return the apropriate video stream to download
@@ -37,21 +38,20 @@ def rename_file(path, old_name, new_name, extension):
         name = new_name + '(' + str(number) + ')'+extension
     os.rename(old_name, path+name)
 
-#Update the progress bar and the percent  progress of the download
-def  progress(stream, chunk=None,bytes_remaining=None):
-    decimal_percent = (stream.filesize-bytes_remaining)/stream.filesize
-    blocks = math.floor(decimal_percent*20)
-    bar = '[' + blocks * chr(9608)+ (20-blocks)*' '+']'
+def print_progress_bar(blocks_number, conclued, total):
+    decimal_percent = conclued/total
+    blocks = math.floor(decimal_percent*blocks_number)
+    bar = '[' + blocks * chr(9608)+ (blocks_number-blocks)*' '+']'
     print(f'{bar} {decimal_percent* 100:.2f}%', end='\r')
 
-#when download is completed
-def complete(stream,file_path = None):
-    print('')
+#Update the progress bar and the percent  progress of the download
+def on_progress(stream, chunk=None,bytes_remaining=None):
+    print_progress_bar(20, stream.filesize-bytes_remaining, stream.filesize)
 
 #Create the video object and download it
-def download_video(url, path, audioOnly):
+def download_video(url, path, audioOnly,progress = None):
     try:
-        video = YouTube(url, on_progress_callback=progress, on_complete_callback=complete)
+        video = YouTube(url, on_progress_callback=progress)
     except:
         print("Something gone wrong with the video url, check if it still exist")
         exit()
@@ -65,34 +65,38 @@ def download_video(url, path, audioOnly):
 
 #Call video_download for one video
 def download_single(data):
-    print(f"Downloading 1 of 1")
-    bar = '[' + 20 * ' ' +']'
-    print(f'{bar} 0.00%', end='\r')
-    download_video(data['url'], data['path'], data['audioOnly'])
-    print("Successfully Completed")
+    print(f"Downloading 1 video")
+    print_progress_bar(20,0,10)
+    download_video(data['url'], data['path'], data['audioOnly'], on_progress)
+    print("\n Successfully Completed")
 
 #Create a playlist object and call download_video for each video
-def dowload_playlist(data):
+def dowload_playlist(data,threads_number):
     try:
         playlist = Playlist(data['url'])
     except:
         print("Something gone wrong with the playlist url, check if it still exist and is not private")
         exit()
     count = 0
-    for video in playlist.video_urls:
-        count+=1
-        print(f"Downloading {count} of {len(playlist.video_urls)}")
-        bar = '[' + 20 * ' ' +']'
-        print(f'{bar} 0.00%', end='\r')
-        download_video(video, data['path'], data['audioOnly'])
-    print("Successfully Completed")
+    print(f"Downloading {len(playlist)} videos")
+    print_progress_bar(50,0,10)
+    futures = []
+    with concurrent.futures.ThreadPoolExecutor(max_workers=threads_number) as executor:
+        for video in playlist.video_urls:
+            futures.append(executor.submit(download_video, url=video, path=data['path'], audioOnly=data['audioOnly']))
+        
+        for future in concurrent.futures.as_completed(futures):
+            count+=1
+            print_progress_bar(50,count,len(playlist))
+
+    print("\n Successfully Completed")
 
 #main funcion
 def main():
     args = create_parser()
     data = {'path':os.path.abspath(args.path)+os.path.sep, 'url':args.url,'isPlaylist':args.playlist,'audioOnly':args.audio}
     if(args.playlist):
-        dowload_playlist(data)
+        dowload_playlist(data,args.multithreading)
     else:   
         download_single(data)
 
