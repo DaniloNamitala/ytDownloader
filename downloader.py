901bb9c15a6e846440879d9342284276882d2692
Danilo Namitala <danilonamitala@yahoo.com.br>
2021-04-13 10:45:34 -0300

    improviments in exceptions handling

--- downloader.py
+++ downloader.py
@@ -32,6 +32,7 @@ def get_stream(video, audioOnly):
 #so this function rename the file to video title appending a number if the file name already exists
 def rename_file(path, old_name, new_name, extension):
     number = 0
+    new_name = new_name.replace('/','')
     name = new_name+extension
     while(os.path.isfile(path+name)):
         number += 1
@@ -52,23 +53,32 @@ def on_progress(stream, chunk=None,bytes_remaining=None):
 def download_video(url, path, audioOnly,progress = None):
     try:
         video = YouTube(url, on_progress_callback=progress)
+    except VideoUnavailable:
+        print(f"o video {url} esta indisponivel")
     except:
         print("Something gone wrong with the video url, check if it still exist")
-        exit()
-    stream = get_stream(video, audioOnly)
-    output_file = stream.download(output_path=path, filename='_'+video.title)
+        return False
+    else:
+        try:
+            stream = get_stream(video, audioOnly)
+        except:
+            print("Something gone wrong with the video url, check if it still exist")
+            return False
+        else:
+            output_file = stream.download(output_path=path, filename='_'+video.title)
 
-    sufix = '.mp4'
-    if(audioOnly):
-        sufix = '.mp3'
-    rename_file(path,output_file, video.title,sufix)
+            sufix = '.mp4'
+            if(audioOnly):
+                sufix = '.mp3'
+            rename_file(path,output_file, video.title,sufix)
 
 #Call video_download for one video
 def download_single(data):
     print(f"Downloading 1 video")
     print_progress_bar(20,0,10)
-    download_video(data['url'], data['path'], data['audioOnly'], on_progress)
-    print("\n Successfully Completed")
+    
+    if(download_video(data['url'], data['path'], data['audioOnly'], on_progress)):
+        print("\nSuccessfully Completed")
 
 #Create a playlist object and call download_video for each video
 def dowload_playlist(data,threads_number):
@@ -89,7 +99,7 @@ def dowload_playlist(data,threads_number):
             count+=1
             print_progress_bar(50,count,len(playlist))
 
-    print("\n Successfully Completed")
+    print("\nSuccessfully Completed")
 
 #main funcion
 def main():
